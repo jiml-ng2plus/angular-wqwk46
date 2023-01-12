b87ab7fb460a3578ca5c50ead42c0ccda0826de3
jiml-ng2plus <36929529+jiml-ng2plus@users.noreply.github.com>
2023-01-12 13:21:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wqwk46
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wqwk46)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {SlideToggleConfigurableExample} from './slide-toggle-configurable-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [SlideToggleConfigurableExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [SlideToggleConfigurableExample],
+})
+export class AppModule {}
